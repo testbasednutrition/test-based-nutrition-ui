b81c19e7bab76e543a74428e959d1d3a42a558ee
testbased <testbasednutritionofficial@gmail.com>
2026-06-26 20:05:44 +0200

    fix: import React for React.Fragment support on TBNMethod page

--- src/pages/TBNMethod.tsx
+++ src/pages/TBNMethod.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
